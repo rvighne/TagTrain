a06a99365b183af7cefb43d114d74f6efc02e6e1
Rohit Vighne <rohit.vighne@gmail.com>
2016-08-16 08:40:45 -0700

    Use <label> as container (instead of custom focus handler)

--- src/tagtrain.js
+++ src/tagtrain.js
@@ -32,7 +32,7 @@ function TagTrain(opts) {
 	this.opts = Object.assign({}, TagTrain.defaultOpts, opts);
 	this.tags = [];
 
-	this.el = document.createElement('div');
+	this.el = document.createElement('label');
 	this.el.classList.add(TagTrain.classNames.container);
 
 	this.tagList = document.createElement('ul');
@@ -51,7 +51,6 @@ function TagTrain(opts) {
 	};
 
 	this.el.addEventListener('click', TagTrain.removeItem.bind(this));
-	this.el.addEventListener('click', TagTrain.focusInput.bind(this));
 	this.input.addEventListener('keydown', TagTrain.tagInput.bind(this));
 }
 
@@ -64,7 +63,9 @@ TagTrain.classNames = {
 	input: 'TagTrain-input'
 };
 
-TagTrain.idPrefix = 'TagTrain-tag-';
+TagTrain.idPrefixes = {
+	tag: 'TagTrain-tag-'
+};
 
 TagTrain.defaultOpts = {
 	delimiters: [9, 13, 32, 188],
@@ -77,13 +78,13 @@ TagTrain.defaultOpts = {
 TagTrain.prototype.on = function on(event, callback) {
 	this.events[event].push(callback);
 	return this;
-}
+};
 
 TagTrain.prototype.off = function off(event, callback) {
 	var callbacks = this.events[event];
 	callbacks.splice(callbacks.indexOf(callback), 1);
 	return this;
-}
+};
 
 TagTrain.prototype.trigger = function trigger(event, data) {
 	var callbacks = this.events[event];
@@ -92,13 +93,13 @@ TagTrain.prototype.trigger = function trigger(event, data) {
 	}
 
 	return this;
-}
+};
 
 TagTrain.prototype.addTag = function addTag(value) {
 	if (value !== '' && this.tags.length < this.opts.maxTags && !this.opts.invalidTag.test(value) && this.tags.indexOf(value) === -1) {
 		var item = document.createElement('li');
 		item.classList.add(TagTrain.classNames.tagItem);
-		item.id = TagTrain.idPrefix + value;
+		item.id = TagTrain.idPrefixes.tag + value;
 		item.appendChild(document.createTextNode(value));
 
 		var removeBtn = document.createElement('span');
@@ -124,10 +125,10 @@ TagTrain.prototype._removeItem = function _removeItem(item) {
 	var oldValue = this.tags.splice(
 		this.tags.indexOf(
 			item.id.slice(
-				TagTrain.idPrefix.length)), 1)[0];
+				TagTrain.idPrefixes.tag.length)), 1)[0];
 
 	this.trigger('remove-tag', oldValue).trigger('change', this.tags);
-}
+};
 
 TagTrain.prototype.removeAll = function removeAll() {
 	var oldTags = this.tags.slice();
@@ -151,19 +152,12 @@ TagTrain.prototype.setTags = function setTags(tags) {
 
 TagTrain.prototype.takeFocus = function takeFocus() {
 	this.input.select();
-}
+};
 
 /* Event handlers -- static, but must be bound to instance before registering */
 TagTrain.removeItem = function removeItem(e) {
 	if (e.target.classList.contains(TagTrain.classNames.removeBtn)) {
 		this._removeItem(e.target.parentNode);
-	}
-};
-
-TagTrain.focusInput = function focusInput(e) {
-	var sel = document.getSelection();
-	if (e.target !== this.input && (sel.isCollapsed || !sel.containsNode(this.el, true))) {
-		this.input.select();
 	}
 };
 
